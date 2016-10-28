69769e647d4c823acb1bae2304aa3d7df766ae08
BelinChung <belinchung@gmail.com>
2016-10-28 10:05:14 +0800

    Index page support pulldown refresh and infinite load

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,12 +2,57 @@ let app = getApp()
 let util = require('../../utils/util')
 let ajax = require('../../network/ajax')
 
+let refreshing = false, refreshed = false, loadingMore = false, loadedEnd = false
+
 Page({
   data: {
     timeline: []
   },
   onReady() {
     this.getTimeline()  
+  },
+  onPullDownRefresh() {
+    if(refreshing) return false
+
+    refreshing = true
+    ajax({
+        url: 'refresh_timeline.json',
+        success: res => {
+          if(refreshed) {
+            wx.showToast({
+              title: '没有刷出新消息哦！'
+            })
+          } else {
+            let timeline = this.formatTimeline(res.data)
+            this.setData({
+              timeline: [...timeline, ...this.data.timeline]
+            })
+          }
+        },
+        complete: _ => {
+          refreshing = false
+          refreshed = true
+          wx.stopPullDownRefresh()
+        }
+    })
+  },
+  scrollToLower() {
+    if(loadingMore || loadedEnd) return false
+
+    loadingMore = true
+    ajax({
+        url: 'more_timeline.json',
+        success: res => {
+          let timeline = this.formatTimeline(res.data)
+          this.setData({
+            timeline: [...this.data.timeline, ...timeline]
+          })
+        },
+        complete: _ => {
+           loadingMore = false
+           loadedEnd = true 
+        }
+    })
   },
   getTimeline() {
     wx.showToast({
