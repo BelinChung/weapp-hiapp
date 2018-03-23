f67546103af9aeea0821c8da3c59b60de6cc487e
BelinChung <belinchung@gmail.com>
2018-03-23 09:43:42 +0800

    Fixed timeline formatting

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,14 @@
+import distanceInWordsToNow from 'date-fns/distance_in_words_to_now'
+
 export function getRemoteAvatar(id) {
   return `https://loremflickr.com/70/70/people?lock=${id}`
+}
+
+export function formatTimeline(items) {
+  items.forEach(item => {
+    item.avatarUrl = getRemoteAvatar(item.avatar)
+    item.time = distanceInWordsToNow(item.created_at * 1000, { addSuffix: true })
+    return item
+  })
+  return items
 }
